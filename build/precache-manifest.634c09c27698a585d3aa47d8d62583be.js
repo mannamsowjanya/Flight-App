52f4235a4dc0c749dfe1b8ce15389887ed4bdd54
unknown <sowjanyamannam@gmail.com>
2021-08-25 12:08:19 +0530

    deployed

---
+++ build/precache-manifest.634c09c27698a585d3aa47d8d62583be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd49121052ae9c6a40a6",
+    "url": "/Flight-App/static/css/main.74e464fc.chunk.css"
+  },
+  {
+    "revision": "bd49121052ae9c6a40a6",
+    "url": "/Flight-App/static/js/main.e933e405.chunk.js"
+  },
+  {
+    "revision": "237d6ca1f4f1ca0786a1",
+    "url": "/Flight-App/static/js/runtime~main.80c7edad.js"
+  },
+  {
+    "revision": "ad8392a91e6115b58885",
+    "url": "/Flight-App/static/css/2.513c9f94.chunk.css"
+  },
+  {
+    "revision": "ad8392a91e6115b58885",
+    "url": "/Flight-App/static/js/2.cd7fce67.chunk.js"
+  },
+  {
+    "revision": "1539bd03e558544b90f6fced3772fbcd",
+    "url": "/Flight-App/static/media/1_1.1539bd03.png"
+  },
+  {
+    "revision": "cb61ab61535123a00300475cbe31af85",
+    "url": "/Flight-App/static/media/2_1.cb61ab61.png"
+  },
+  {
+    "revision": "45b313efe02f71f5002301aeefd753cf",
+    "url": "/Flight-App/static/media/3_1.45b313ef.png"
+  },
+  {
+    "revision": "001ee04e4a0951bc261467366c13b879",
+    "url": "/Flight-App/static/media/4_1.001ee04e.png"
+  },
+  {
+    "revision": "8a9f1caf802b4123c2121ad2adbfccca",
+    "url": "/Flight-App/index.html"
+  }
+];
